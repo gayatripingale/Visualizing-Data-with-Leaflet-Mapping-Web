4dcb7d2ad013217147a936ef13a0f5f381bbe30b
Gayatri Pingale <gayatripingale@gmail.com>
2018-05-18 11:06:52 -0500

    adding css,html,image fiels

---
+++ logic.js
@@ -0,0 +1,3 @@
+// store our API endpoint inside querry URL //
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var query2 = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson"
